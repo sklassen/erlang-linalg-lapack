264a6a6975ad5ddb682101f6d64c15defde07f5e
Simon Klassen <simon@mohawk.localdomain>
2019-01-16 15:27:59 +0900

    eunit test works for multiplication

---
+++ test/matrix_lapack_tests.erl
@@ -0,0 +1,16 @@
+-module(matrix_lapack_tests). 
+-import(matrix_lapack,[transpose/1,multiply/2]).
+-include_lib("eunit/include/eunit.hrl").
+
+%transpose_1_test() ->
+%	?assert(transpose([[8.0]])=:=[[8.0]]).
+%
+%transpose_2_test() ->
+%	?assert(transpose([[1.0,2.0],[3.0,4.0]])==[[1.0,3.0],[2.0,4.0]]).
+%
+%transpose_3_test() ->
+%	?assert(transpose([[1.0,2.0,3.0],[4.0,5.0,6.0]])==[[1.0,4.0],[2.0,5.0],[3.0,6.0]]).
+
+multiply_2_test()->
+	?assert(multiply([[1.0,2.0],[3.0,4.0]],[[1.0,3.0],[2.0,4.0]])==[[5.0,11.0],[11.0,25.0]]).
+
